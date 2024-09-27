980878c67a880646f393e21174935724c0c2413f
Marcus Funch <mfm@magenta.dk>
2024-09-27 17:27:19 +0200

    Add dedicated activate arguments to shutdown and wakeup scripts

--- common/system/shutdown_at_time.sh
+++ common/system/shutdown_at_time.sh
@@ -1,13 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # SYNOPSIS
-#    shutdown_at_time.sh <hours> <minutes>
+#    shutdown_at_time.sh <activate> <hours> <minutes>
 #
 # DESCRIPTION
 #    This is a script to make a OS2BorgerPC machine shutdown at a certain time.
-#
-#    To disable the scheduled shutdown:
-#      shutdown_at_time.sh --off
 #
 #    We'll suppose the user only wants to have regular shutdown once a day
 #    as specified by the <hours> and <minutes> parameters. Thus, any line in
@@ -21,67 +18,75 @@
 
 set -x
 
+ACTIVATE=$1
+HOURS=$2
+MINUTES=${3:-0}
+
 WAKE_PLAN_FILE=/etc/os2borgerpc/plan.json
 OUR_USER="user"
+ROOTCRON_TMP=/tmp/oldcron
+USERCRON=/etc/os2borgerpc/usercron
+USER_CLEANUP=/usr/share/os2borgerpc/bin/user-cleanup.bash
 
 if [ -f $WAKE_PLAN_FILE ]; then
   echo "Dette script kan ikke anvendes på en PC, der er tilknyttet en tænd/sluk tidsplan."
   exit 1
 fi
 
-ROOTCRON_TMP=/tmp/oldcron
-USERCRON=/etc/os2borgerpc/usercron
-if grep "LANG=" /etc/default/locale | grep "sv"; then
-  MESSAGE="Den här datorn stängs av om fem minuter"
-elif grep "LANG=" /etc/default/locale | grep "en"; then
-  MESSAGE="This computer will shut down in five minutes"
-else
+if grep "LANG=" /etc/default/locale | grep "da"; then
   MESSAGE="Denne computer lukker ned om fem minutter"
+elif grep "LANG=" /etc/default/locale | grep "sv"; then
+  MESSAGE="Den här datorn stängs av om fem minuter"
+else
+  MESSAGE="This computer will shut down in five minutes"
 fi
 
 # Read and save current cron settings first
 crontab -l > $ROOTCRON_TMP
 
 # Ensure that the usercron-file exists and has the correct permissions
-touch $USERCRON
-chmod 700 $USERCRON
+if id $OUR_USER > /dev/null 2>&1; then
+  touch $USERCRON
+  chmod 700 $USERCRON
+
+  # Delete current crontab entries related to this script AND shutdown_and_wakeup.sh
+  sed --in-place "/notify-send/d" $USERCRON
+fi
 
 # Delete current crontab entries related to this script AND shutdown_and_wakeup.sh
 sed --in-place --expression "/shutdown/d" --expression "/rtcwake/d" --expression "/scheduled_off/d" $ROOTCRON_TMP
-sed --in-place "/notify-send/d" $USERCRON
 
-# If not called with --off: Determine the new crontab contents
-if [ "$1" != "--off" ]; then
+if [ "$ACTIVATE" = "True" ]; then
 
-    if [ $# == 2 ]; then
-        HOURS=$1
-        MINUTES=${2:-0}
-        # Assume the parameters are already validated as integers.
-        echo "$MINUTES $HOURS * * * /sbin/shutdown -P now" >> $ROOTCRON_TMP
+  if [ $# -lt 3 ]; then
+    echo "Usage: shutdown_at_time.sh [activate] [hours] [minutes]"
+  else
+    # Assume the parameters are already validated as integers.
+    echo "$MINUTES $HOURS * * * /sbin/shutdown --poweroff now" >> $ROOTCRON_TMP
 
-        MINM5P60=$(( $(( MINUTES - 5)) + 60))
-        # Rounding minutes
-        MINS=$(( MINM5P60 % 60))
-        HRCORR=$(( 1 - $(( MINM5P60 / 60))))
-        HRS=$(( HOURS - HRCORR))
-        HRS=$(( $(( HRS + 24)) % 24))
-        # Now output to user's crontab as well
-        echo "$MINS $HRS * * * XDG_RUNTIME_DIR=/run/user/\$(id -u) /usr/bin/notify-send \"$MESSAGE\"" >> $USERCRON
-    else
-        echo "Usage: shutdown_at_time.sh [--off] [hours minutes]"
+    # BorgerPC-specific logic
+    if id $OUR_USER > /dev/null 2>&1; then
+      MINM5P60=$(( $(( MINUTES - 5)) + 60))
+      # Rounding minutes
+      MINS=$(( MINM5P60 % 60))
+      HRCORR=$(( 1 - $(( MINM5P60 / 60))))
+      HRS=$(( HOURS - HRCORR))
+      HRS=$(( $(( HRS + 24)) % 24))
+      # Now output to user's crontab as well
+      echo "$MINS $HRS * * * XDG_RUNTIME_DIR=/run/user/\$(id -u) /usr/bin/notify-send \"$MESSAGE\"" >> $USERCRON
     fi
+  fi
 fi
 
 # Update crontabs accordingly - either with an empty crontab or updated ones
 crontab $ROOTCRON_TMP || exit 1
+rm --force $ROOTCRON_TMP
 
 if id $OUR_USER > /dev/null 2>&1; then
   crontab -u $OUR_USER $USERCRON || exit 1
-fi
 
-# Ensure that user-cleanup resets the user crontab
-if [ -f "$USER_CLEANUP" ] && ! grep --quiet "crontab" "$USER_CLEANUP"; then
-  echo "crontab -u $OUR_USER $USERCRON" >> "$USER_CLEANUP"
+  # Ensure that user-cleanup resets the user crontab
+  if ! grep --quiet "crontab" "$USER_CLEANUP"; then
+    echo "crontab -u $OUR_USER $USERCRON" >> "$USER_CLEANUP"
+  fi
 fi
-
-rm --force $ROOTCRON_TMP
