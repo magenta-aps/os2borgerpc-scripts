0926c47466b66ddbf6dc57afebf4e3efc03a0d21
Marcus Funch <mfm@magenta.dk>
2024-10-11 14:04:10 +0200

    Refactor firefox_global_policies

--- os2borgerpc/browser/firefox_global_policies.sh
+++ os2borgerpc/browser/firefox_global_policies.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 : << 'COMMENT'
 Policy-script developed by Magenta ApS for Aarhus Municipal.
+
 Learn more about Firefox "Policy Names" here:
 https://github.com/mozilla/policy-templates/blob/master/README.md
-It's only possible to have ONE policy-file. In the future this script 
-should have to evolve to be a more dynamic solution if we want to be 
-able to, e.g. use the same script accross machines and handpick which
+
+It's only possible to have ONE policy-file. In the future this script
+should have to evolve to be a more dynamic solution if we want to be
+able to, e.g. use the same script across machines and handpick which
 Policies we want to use. Until then there will be set some default static
-Policies with OS2borgerPC in mind.  
+Policies with OS2borgerPC in mind.
+
 Author: Heini L. Ovason
 COMMENT
 
 set -x
 
 if get_os2borgerpc_config os2_product | grep --quiet kiosk; then
-  echo "Dette script er ikke designet til at blive anvendt på en kiosk-maskine."
+  echo "This script is not designed to be used on a kiosk machine."
   exit 1
 fi
 
@@ -23,31 +26,28 @@ STARTPAGE="$1"
 ADDITIONAL_PAGES="$2"
 
 POLICY_DIR="/etc/firefox/policies"
-POLICY_FILE="policies.json"
+POLICY_FILE="$POLICY_DIR/policies.json"
 
 if [ -z "$STARTPAGE" ]; then
   echo "WARNING: Missing <URL> argument. Not able to set Firefox startpage."
   exit 1
 fi
 
-if [ ! -d "$POLICY_DIR" ]; then
-  mkdir -p "$POLICY_DIR";
-fi
+mkdir --parents "$POLICY_DIR"
 
 PAGES_STRING=""
 if [ -n "$ADDITIONAL_PAGES" ]; then
   IFS='|' read -ra PAGES_ARRAY <<< "$ADDITIONAL_PAGES"
 
   PAGES_STRING="\"Additional\": [" # start array-string
-  for PAGE in "${PAGES_ARRAY[@]}"
-  do
-      PAGES_STRING+="\"$PAGE\","
+  for PAGE in "${PAGES_ARRAY[@]}"; do
+    PAGES_STRING+="\"$PAGE\","
   done
   PAGES_STRING=${PAGES_STRING::-1} # remove comma at end of list
   PAGES_STRING+="]," # finish array-string
 fi
 
-cat << EOF > "$POLICY_DIR/$POLICY_FILE"
+cat << EOF > "$POLICY_FILE"
 {
   "policies": {
     "Homepage": {
@@ -66,18 +66,18 @@ cat << EOF > "$POLICY_DIR/$POLICY_FILE"
       "datareporting.policy.dataSubmissionPolicyBypassNotification": true
     },
     "BlockAboutAddons": true,
-	  "BlockAboutConfig": true,
-	  "BlockAboutProfiles": true,
-	  "BlockAboutSupport": true,
+    "BlockAboutConfig": true,
+    "BlockAboutProfiles": true,
+    "BlockAboutSupport": true,
     "DownloadDirectory": "/home/user/Hentet",
     "PromptForDownloadLocation": false,
-	  "DisableFirefoxAccounts": true,
-	  "DisableFormHistory": true,
-	  "DisableProfileImport": true,
+    "DisableFirefoxAccounts": true,
+    "DisableFormHistory": true,
+    "DisableProfileImport": true,
     "OfferToSaveLogins": false,
-	  "OfferToSaveLoginsDefault": false,
-	  "PasswordManagerEnabled": false,
-	  "SanitizeOnShutdown": {
+    "OfferToSaveLoginsDefault": false,
+    "PasswordManagerEnabled": false,
+    "SanitizeOnShutdown": {
       "Cache": true,
       "Cookies": true,
       "Downloads": false,
@@ -100,11 +100,7 @@ cat << EOF > "$POLICY_DIR/$POLICY_FILE"
     "DisableDeveloperTools": true
   }
 }
-
 EOF
 
 # Attempting to remove policy from former standard location.
-OLD_POLICY="/usr/lib/firefox/distribution/policies.json"
-if [ -f "$OLD_POLICY" ]; then
-    rm -f "$OLD_POLICY"
-fi
+rm --force /usr/lib/firefox/distribution/policies.json
