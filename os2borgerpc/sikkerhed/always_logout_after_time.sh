d3e102090910ec425ffce578f9a02768caf9dcf0
Marcus Funch <mfm@magenta.dk>
2024-10-15 09:56:30 +0200

    Additional changes (will rebase before merge)

--- os2borgerpc/sikkerhed/always_logout_after_time.sh
+++ os2borgerpc/sikkerhed/always_logout_after_time.sh
@@ -1,59 +1,58 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Logout the user from the graphical user interface after N minutes.
-# Takes exactly one parameter.
 
 if get_os2borgerpc_config os2_product | grep --quiet kiosk; then
   echo "Dette script er ikke designet til at blive anvendt på en kiosk-maskine."
   exit 1
 fi
 
-if [ $# -ne 1 ]; then
-    echo "This job takes exactly one parameter."
+if [ $# -ne 2 ]; then
+    echo "This script takes exactly two parameters."
     exit 1
 fi
 
+ACTIVATE=$1
+LOGIN_SESSION_LENGTH_IN_MINS=$2
+
 AUTO_LOGOUT_DESKTOP_FILE="/home/.skjult/.config/autostart/auto_logout.sh.desktop"
 AUTO_LOGOUT_SCHEDULE_SCRIPT="/usr/share/os2borgerpc/bin/auto_logout.sh"
+export DEBIAN_FRONTEND=noninteractive
 
-if grep "LANG=" /etc/default/locale | grep "da"; then
+if grep "LANG=" /etc/default/locale | grep --quiet "da"; then
   MESSAGE="Du vil blive logget ud om fem minutter"
-elif grep "LANG=" /etc/default/locale | grep "sv"; then
+elif grep "LANG=" /etc/default/locale | grep --quiet "sv"; then
   MESSAGE="Du kommer att loggas ut om fem minuter"
 else
   MESSAGE="You will be logged out in five minutes"
 fi
 
-# Install at
-dpkg -l at > /dev/null 2>&1
-HAS_AT=$?
-
-if [[ $HAS_AT == 1 ]]; then
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get update
-    apt-get install --assume-yes at
-fi
-
-if [[ ($1 == "--disable") || ($1 == "0") ]]; then
+if [ "$ACTIVATE" = "False" ]; then
     # Clean up
     rm --force $AUTO_LOGOUT_SCHEDULE_SCRIPT $AUTO_LOGOUT_DESKTOP_FILE
     atq | cut --fields 1 | xargs --no-run-if-empty atrm
     exit 0
 fi
 
+# Install "at" if it isn't already
+if ! dpkg -l at > /dev/null 2>&1; then
+    apt-get update
+    apt-get install --assume-yes at
+fi
+
 cat <<- EOF > $AUTO_LOGOUT_SCHEDULE_SCRIPT
 	#!/usr/bin/env bash
 
-  atq | cut --fields 1 | xargs --no-run-if-empty atrm
+	atq | cut --fields 1 | xargs --no-run-if-empty atrm
 
-	TIME=$1
+	LOGIN_SESSION_LENGTH_IN_MINS=$LOGIN_SESSION_LENGTH_IN_MINS
 
-	if [ \$TIME -ge 5 ]; then
-	    TM5=\$(expr \$TIME - 5)
+	if [ \$LOGIN_SESSION_LENGTH_IN_MINS -ge 5 ]; then
+	    TM5=\$(expr \$LOGIN_SESSION_LENGTH_IN_MINS - 5)
 	    echo 'DISPLAY=:0.0 XAUTHORITY=/home/user/.Xauthority /usr/bin/zenity --warning --text="$MESSAGE"' | at now + \$TM5 min
 	fi
 
-	echo "kill -9 -1" | at now + \$TIME min
+	echo "kill -9 -1" | at now + \$LOGIN_SESSION_LENGTH_IN_MINS min
 EOF
 
 mkdir --parents /home/.skjult/.config/autostart
@@ -69,5 +68,3 @@ cat <<- EOF > $AUTO_LOGOUT_DESKTOP_FILE
 EOF
 
 chmod +x $AUTO_LOGOUT_DESKTOP_FILE $AUTO_LOGOUT_SCHEDULE_SCRIPT
-
-exit 0
