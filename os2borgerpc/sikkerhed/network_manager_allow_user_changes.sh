e7dfb8ce5a1e512ea9427fb07d2ef3fbc2439b44
Andreas Poulsen <ap@magenta-aps.dk>
2024-10-17 15:24:28 +0200

    Make network_manager_allow_user_changes use new polkit format in 24.04

--- os2borgerpc/sikkerhed/network_manager_allow_user_changes.sh
+++ os2borgerpc/sikkerhed/network_manager_allow_user_changes.sh
@@ -21,20 +21,29 @@ NETWORK_MANAGER_CONF=/etc/NetworkManager/NetworkManager.conf
 # We used to use /var/lib for this one policy, whereas /etc were used for the rest. /etc/ takes precedence over
 # /var/lib, so use that instead
 NM_POLKIT_OLD=/var/lib/polkit-1/localauthority/50-local.d/networkmanager.pkla
-NM_POLKIT_NEW=/etc/polkit-1/localauthority/50-local.d/networkmanager.pkla
-mkdir --parents "$(dirname $NM_POLKIT_NEW)"
+NM_POLKIT_LEGACY=/etc/polkit-1/localauthority/50-local.d/networkmanager.pkla
+NM_POLKIT=/etc/polkit-1/rules.d/10-networkmanager.rules
+mkdir --parents "$(dirname $NM_POLKIT_LEGACY)"
 
 if [ -f $NM_POLKIT_OLD ]; then
-  mv $NM_POLKIT_OLD $NM_POLKIT_NEW
+  mv $NM_POLKIT_OLD $NM_POLKIT_LEGACY
 fi
 
 # Cleanup after previous runs of this script - or disable access if previously given (idempotency)
-sed --in-place '/auth-polkit=false/d' $NETWORK_MANAGER_CONF
-# Only make this replacement for user-related entries
-sed --in-place '/unix-group:user/{ n; n; n; n; s/ResultActive=yes/ResultActive=no/ }' $NM_POLKIT_NEW
+if [ -f $NM_POLKIT ]; then # 24.04 support
+  sed --in-place 's/var users =.*/var users = ["user", "gdm", "lightdm"]/' $NM_POLKIT
+else # 20.04 and 22.04 support
+  sed --in-place '/auth-polkit=false/d' $NETWORK_MANAGER_CONF
+  # Only make this replacement for user-related entries
+  sed --in-place '/unix-group:user/{ n; n; n; n; s/ResultActive=yes/ResultActive=no/ }' $NM_POLKIT_LEGACY
+fi
 
 if [ "$ACTIVATE" = 'True' ]; then
-  sed --in-place '/\[main\]/a\auth-polkit=false' $NETWORK_MANAGER_CONF
-  # Only make this replacement for user-related entries
-  sed --in-place '/unix-group:user/{ n; n; n; n; s/ResultActive=no/ResultActive=yes/ }' $NM_POLKIT_NEW
+  if [ -f $NM_POLKIT ]; then # 24.04 support
+    sed --in-place 's/var users =.*/var users = ["gdm", "lightdm"]/' $NM_POLKIT
+  else
+    sed --in-place '/\[main\]/a\auth-polkit=false' $NETWORK_MANAGER_CONF
+    # Only make this replacement for user-related entries
+    sed --in-place '/unix-group:user/{ n; n; n; n; s/ResultActive=no/ResultActive=yes/ }' $NM_POLKIT_LEGACY
+  fi
 fi
