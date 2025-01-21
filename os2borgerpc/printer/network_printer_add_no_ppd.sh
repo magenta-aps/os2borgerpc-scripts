b22cf665804c85c3fcd829710fff2e6930d560f0
Andreas Poulsen <ap@magenta-aps.dk>
2025-01-21 16:15:26 +0100

    Minor change to network_printer_add_no_ppd

--- os2borgerpc/printer/network_printer_add_no_ppd.sh
+++ os2borgerpc/printer/network_printer_add_no_ppd.sh
@@ -15,7 +15,7 @@ DESCRIPTION="$3"
 PROTOCOL="${4:-ipp}"
 SET_STANDARD="$5"
 
-[ "$PROTOCOL" = "ipp" ] && ENABLE_IPP_EVERYWHERE="-m everywhere"
+[ "$PROTOCOL" = "ipp" ] || [ "$PROTOCOL" = "ipps" ] && ENABLE_IPP_EVERYWHERE="-m everywhere"
 
 # shellcheck disable=SC2086  # We want word-splitting in the last argument
 lpadmin -p "$NAME" -v "$PROTOCOL://$HOST" -D "$DESCRIPTION" -L "$DESCRIPTION" -E $ENABLE_IPP_EVERYWHERE
