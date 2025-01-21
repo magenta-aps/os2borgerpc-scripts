06cd97880ac23588df6ab3e9169af48bc98c3f90
Andreas Poulsen <ap@magenta-aps.dk>
2025-01-21 13:46:52 +0100

    Make the automatic login scripts work with gdm

--- os2borgerpc/login/change_login_timeout.sh
+++ os2borgerpc/login/change_login_timeout.sh
@@ -16,4 +16,12 @@ if get_os2borgerpc_config os2_product | grep --quiet kiosk; then
   exit 1
 fi
 
-sed --in-place "s/\(autologin-user-timeout=\).*/\1$NEW_TIMEOUT_IN_SECONDS/" /etc/lightdm/lightdm.conf
+GDM_AUTOLOGIN_SCRIPT="/usr/share/os2borgerpc/bin/gdm-automatic-login.sh"
+LIGHTDM_CONFIG="/etc/lightdm/lightdm.conf"
+DEFAULT_DM_FILE="/etc/X11/default-display-manager"
+
+if grep --quiet gdm3 $DEFAULT_DM_FILE; then
+  sed --in-place "s/sleep .*/sleep $NEW_TIMEOUT_IN_SECONDS/" $GDM_AUTOLOGIN_SCRIPT
+else
+  sed --in-place "s/\(autologin-user-timeout=\).*/\1$NEW_TIMEOUT_IN_SECONDS/" $LIGHTDM_CONFIG
+fi
--- os2borgerpc/login/user_automatic_login.sh
+++ os2borgerpc/login/user_automatic_login.sh
@@ -10,7 +10,7 @@
 #     1. True will enable automatic login while an unchecked one will disable it.
 #     2. If the first argument is True, this determines if OUR_USER is required to type in their password or not.
 
-set -ex
+set -x
 
 if get_os2borgerpc_config os2_product | grep --quiet kiosk; then
   echo "Dette script er ikke designet til at blive anvendt på en kiosk-maskine."
@@ -19,6 +19,10 @@ fi
 
 LIGHTDM_CONFIG="/etc/lightdm/lightdm.conf"
 OUR_USER="user"
+DEFAULT_DM_FILE="/etc/X11/default-display-manager"
+GDM_CONFIG="/etc/gdm3/custom.conf"
+POST_SESSION_FILE="/etc/gdm3/PostSession/Default"
+GDM_AUTOLOGIN_SCRIPT="/usr/share/os2borgerpc/bin/gdm-automatic-login.sh"
 
 ACTIVATE="$1"
 REQUIRE_PASSWORD="$2"
@@ -26,16 +30,37 @@ REQUIRE_PASSWORD="$2"
 adduser $OUR_USER nopasswdlogin
 
 if [ "$ACTIVATE" = "False" ]; then
-    if [ "$REQUIRE_PASSWORD" = "True" ]; then
-        # Require password for User
-        if id --name --groups $OUR_USER | grep --quiet --word-regexp nopasswdlogin; then
-            # Remove the user from nopasswdlogin group
-            deluser $OUR_USER nopasswdlogin
-        fi
+  if [ "$REQUIRE_PASSWORD" = "True" ]; then
+    # Require password for User
+    if id --name --groups $OUR_USER | grep --quiet --word-regexp nopasswdlogin; then
+      # Remove the user from nopasswdlogin group
+      deluser $OUR_USER nopasswdlogin
     fi
-    # Disable autmatic login
+  fi
+    # Disable automatic login
     sed --in-place "/autologin-user/d" $LIGHTDM_CONFIG
+    sed --in-place "/AutomaticLogin/d" $GDM_CONFIG
+    sed --in-place "\@$GDM_AUTOLOGIN_SCRIPT@d" $POST_SESSION_FILE
+    rm --force $GDM_AUTOLOGIN_SCRIPT
 else # Enable automatic login incl. not requiring password from user on manual login before the timeout
+  if grep --quiet gdm3 $DEFAULT_DM_FILE; then
+    # Idempotency check
+    if ! grep --quiet -- "AutomaticLogin=$OUR_USER" $GDM_CONFIG; then
+      cat << EOF > $GDM_AUTOLOGIN_SCRIPT
+#!/usr/bin/env bash
+
+sleep 10
+if [ -z \$(users) ]; then
+  systemctl restart gdm
+fi
+EOF
+
+      chmod 700 $GDM_AUTOLOGIN_SCRIPT
+
+      sed --in-place "/exit 0/i $GDM_AUTOLOGIN_SCRIPT &" $POST_SESSION_FILE
+      sed --in-place "/daemon/a AutomaticLoginEnable=true\nAutomaticLogin=$OUR_USER" $GDM_CONFIG
+    fi
+  else
     # Idempotency check
     if ! grep --quiet -- "autologin-user=$OUR_USER" $LIGHTDM_CONFIG; then
 			cat <<- EOF >> $LIGHTDM_CONFIG
@@ -43,4 +68,5 @@ else # Enable automatic login incl. not requiring password from user on manual l
 				autologin-user=$OUR_USER
 			EOF
     fi
+  fi
 fi
