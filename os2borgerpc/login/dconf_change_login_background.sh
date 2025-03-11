7f62043c2262052ed71125ee0bc0f262875c4056
Marcus Funch <mfm@magenta.dk>
2025-03-11 17:49:10 +0100

    Update script to change login bg to work with GDM

--- os2borgerpc/login/dconf_change_login_background.sh
+++ os2borgerpc/login/dconf_change_login_background.sh
@@ -17,34 +17,51 @@ fi
 
 ACTIVATE=$1
 IMAGE_UPLOAD=$2
+
 IMAGE_NAME=$(basename "$IMAGE_UPLOAD")
+POLICY_VALUE="/usr/share/backgrounds/$IMAGE_NAME"
+DEFAULT_DM_FILE="/etc/X11/default-display-manager"
 
-mv "$IMAGE_UPLOAD" "/usr/share/backgrounds/"
+# Set the policy for the gdm user, if using GDM, or the os2borgerpc user, if using LightDM
+if grep --quiet gdm3 $DEFAULT_DM_FILE; then
+  DCONF_USER_DIR="gdm.d"
+  POLICY_PATH="com/ubuntu/login-screen"
+  POLICY="background-picture-uri"
+else
+  DCONF_USER_DIR="os2borgerpc.d"
+  POLICY_PATH="com/canonical/unity-greeter"
+  POLICY="background"
+fi
 
-# Change these three to set a different policy to another value
-POLICY_PATH="com/canonical/unity-greeter"
-POLICY="background"
-POLICY_VALUE="'/usr/share/backgrounds/$IMAGE_NAME'"
-
-POLICY_FILE="/etc/dconf/db/os2borgerpc.d/06-login-screen-bg-image"
-POLICY_LOCK_FILE="/etc/dconf/db/os2borgerpc.d/locks/06-login-screen-bg-image"
-
+POLICY_FILE="/etc/dconf/db/$DCONF_USER_DIR/06-login-screen-bg-image"
+POLICY_LOCK_FILE="/etc/dconf/db/$DCONF_USER_DIR/locks/06-login-screen-bg-image"
 
 if [ "$ACTIVATE" = "True" ]; then
 
-	cat > "$POLICY_FILE" <<-END
-		[$POLICY_PATH]
-		draw-user-backgrounds=false
-		$POLICY=$POLICY_VALUE
-	END
-	# Tell the system that the values of the dconf keys we've just set can no
-	# longer be overridden by the user
-	cat > "$POLICY_LOCK_FILE" <<-END
+  mv "$IMAGE_UPLOAD" "/usr/share/backgrounds/"
+
+	if grep --quiet gdm3 $DEFAULT_DM_FILE; then
+		cat > "$POLICY_FILE" <<-EOF
+			[$POLICY_PATH]
+			background-size='contain'
+			$POLICY='file://$POLICY_VALUE'
+		EOF
+  else
+		cat > "$POLICY_FILE" <<-EOF
+			[$POLICY_PATH]
+			draw-user-backgrounds=false
+			$POLICY='$POLICY_VALUE'
+		EOF
+  fi
+
+  # Tell the system that the values of the dconf keys we've just set can no
+  # longer be overridden by the user
+	cat > "$POLICY_LOCK_FILE" <<-EOF
 		/$POLICY_PATH/$POLICY
-	END
+	EOF
 else
-	rm --force "$POLICY_FILE" "$POLICY_LOCK_FILE"
-    rm POLICY_VALUE
+  # Leaving the image file there
+  rm --force "$POLICY_FILE" "$POLICY_LOCK_FILE"
 fi
 
 # Incorporate all of the text files we've just created into the system's dconf databases
