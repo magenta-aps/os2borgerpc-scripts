29bbe6499006d738a319cda5c0e79b598f773764
Marcus Funch <mfm@magenta.dk>
2026-01-19 15:57:49 +0100

    FF comments

--- os2borgerpc/browser/firefox_global_policies.sh
+++ os2borgerpc/browser/firefox_global_policies.sh
@@ -5,21 +5,17 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 # SPDX-FileContributor: Heini Leander Ovason, Marcus Funch
-
-: << 'COMMENT'
-Policy-script developed by Magenta ApS for Aarhus Municipality.
-
-Learn more about Firefox "Policy Names" here:
-https://github.com/mozilla/policy-templates/blob/master/README.md
-
-It's only possible to have ONE policy-file. In the future this script
-should have to evolve to be a more dynamic solution if we want to be
-able to, e.g. use the same script across machines and handpick which
-Policies we want to use. Until then there will be set some default static
-Policies with OS2borgerPC in mind.
-
-TODO: Add some background for why we have each policy
-COMMENT
+#
+#Policy-script developed by Magenta ApS for Aarhus Municipality.
+#
+#Learn more about Firefox "Policy Names" here:
+#https://github.com/mozilla/policy-templates/blob/master/README.md
+#
+#It's only possible to have ONE policy-file. In the future this script
+#should have to evolve to be a more dynamic solution if we want to be
+#able to, e.g. use the same script across machines and handpick which
+#Policies we want to use. Until then there will be set some default static
+#Policies with OS2borgerPC in mind.
 
 set -x
 
@@ -150,6 +146,9 @@ else
 fi
 
 
+# Disabling the builtin PDF viewer since Princh report it results in bad colours when printing
+# The rest is generally to lock down settings, disable data persistence and remove ads/sponsorships
+# TODO: Add some additional background for why we have each policy
 cat << EOF > "$POLICY_FILE"
 {
   "policies": {
