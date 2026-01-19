b1b7dd08286d54e64f53545161f69745822d5e31
Marcus Funch <mfm@magenta.dk>
2026-01-19 15:59:21 +0100

    FF comments

--- os2borgerpc/browser/firefox_global_policies.sh
+++ os2borgerpc/browser/firefox_global_policies.sh
@@ -6,16 +6,16 @@
 #
 # SPDX-FileContributor: Heini Leander Ovason, Marcus Funch
 #
-#Policy-script developed by Magenta ApS for Aarhus Municipality.
-#
-#Learn more about Firefox "Policy Names" here:
-#https://github.com/mozilla/policy-templates/blob/master/README.md
-#
-#It's only possible to have ONE policy-file. In the future this script
-#should have to evolve to be a more dynamic solution if we want to be
-#able to, e.g. use the same script across machines and handpick which
-#Policies we want to use. Until then there will be set some default static
-#Policies with OS2borgerPC in mind.
+# Policy-script developed by Magenta ApS for Aarhus Municipality.
+# 
+# Learn more about Firefox "Policy Names" here:
+# https://github.com/mozilla/policy-templates/blob/master/README.md
+# 
+# It's only possible to have ONE policy-file. In the future this script
+# should have to evolve to be a more dynamic solution if we want to be
+# able to, e.g. use the same script across machines and handpick which
+# Policies we want to use. Until then there will be set some default static
+# Policies with OS2borgerPC in mind.
 
 set -x
 
