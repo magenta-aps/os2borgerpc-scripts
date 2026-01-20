941ca649d48cff3646d4715f83c0bcd24e58ac6e
Andreas Poulsen <ap@magenta-aps.dk>
2026-01-20 12:09:09 +0100

    Make printer_princh_add correctly set default printer or not

--- os2borgerpc/printer/printer_princh_add.sh
+++ os2borgerpc/printer/printer_princh_add.sh
@@ -28,16 +28,18 @@ lpadmin -x "$NAME" || true
 
 # No princh-cloud-printer binary in path, so checking for princh-setup
 if which princh-setup > /dev/null; then
-   # The two driver options are ISO and US, determining the paper sizes it uses, and specifically whether princheu.ppd (ISO) or princhus.ppd (US) is being used
-   # shellcheck disable=SC2086  # With quotes around princh_setup gets an explicitly empty argument and fails because of it
-   princh-setup add --name "$NAME" --device-id "$PRINCH_ID" --driver iso --description "$DESCRIPTION" $DEFAULT_MAYBE
-   # Finally additionally set the location on the newly added printer
-   lpadmin -p "$NAME" -L "$DESCRIPTION"
-   # Also set it as default via lpoptions as otherwise the default isn't respected for all printers
-   lpoptions -d "$NAME"
+  # The two driver options are ISO and US, determining the paper sizes it uses, and specifically whether princheu.ppd (ISO) or princhus.ppd (US) is being used
+  # shellcheck disable=SC2086  # With quotes around princh_setup gets an explicitly empty argument and fails because of it
+  princh-setup add --name "$NAME" --device-id "$PRINCH_ID" --driver iso --description "$DESCRIPTION" $DEFAULT_MAYBE
+  # Finally additionally set the location on the newly added printer
+  lpadmin -p "$NAME" -L "$DESCRIPTION"
+  if [ "$SET_DEFAULT" = "True" ]; then
+    # Also set it as default via lpoptions as otherwise the default isn't respected for all printers
+    lpoptions -d "$NAME"
+  fi
 else
-   echo "Princh is not installed. Please run the script that installs Princh before this one."
-   exit 1
+  echo "Princh is not installed. Please run the script that installs Princh before this one."
+  exit 1
 fi
 
 echo "Show the current default printer - for verification:"
